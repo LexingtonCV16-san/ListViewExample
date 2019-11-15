20740c196bbfb364084dfd32335c80431b3f46a7
Lexington CV16 <57691928+LexingtonCV16-san@users.noreply.github.com>
2019-11-15 21:35:37 +0800

    Update Player.java

--- app/src/main/java/com/example/listviewexample/Player.java
+++ app/src/main/java/com/example/listviewexample/Player.java
@@ -1,20 +1,31 @@
 package com.example.listviewexample;
 
+import android.widget.ImageView;
+
 public class Player {
     String name;
     int age;
     double worth;
     String main_sport;
-
+    ImageView picture;
     //Todo finish the class player
         // Todo 5 member variables
         //Todo name, age, worth(money), main sport and int image resource from drawable
 
-    public Player(String name,int age,double worth,String main_sport) {
+    public ImageView getPicture() {
+        return picture;
+    }
+
+    public void setPicture(ImageView picture) {
+        this.picture = picture;
+    }
+
+    public Player(String name, int age, double worth, String main_sport, ImageView picture) {
         this.name = name;
         this.age = age;
         this.worth = worth;
         this.main_sport=main_sport;
+        this.picture=picture;
     }
     public void setName(String name) {
         this.name = name;
