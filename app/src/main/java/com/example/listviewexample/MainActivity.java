2b9f290926ec62eddba36ffe797d39906134fd4a
Lexington CV16 <57691928+LexingtonCV16-san@users.noreply.github.com>
2019-11-17 13:56:54 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity  {
     int age;
     double worth;
     MyAdapter myArrayAdapter;
+    ImageView a,b,c,d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +32,15 @@ public class MainActivity extends AppCompatActivity  {
     }
     //Todo  create a method getPlayers() that will create an array of 15 players and return the list of type List<Player>
     public ArrayList<Player> getPlayers(){
-        ArrayList<Player> player = new ArrayList<Player>();
-        player.add(new Player("XiaoHong", 18, 2.5, "tennis"));
-        player.add(new Player("XiaoMing",80, 0,"Computer Game Player"));
-        player.add(new Player("XiaoLan",50, 3.2, "Chess"));
-        player.add(new Player("XiaoLan",20,5.7, "Card"));
+         ArrayList<Player> player = new ArrayList<Player>();
+        a.setImageResource(R.drawable.card);
+        b.setImageResource(R.drawable.chess);
+        c.setImageResource(R.drawable.gamer);
+        d.setImageResource(R.drawable.tennis);
+        player.add(new Player("XiaoHong", 18, 2.5, "tennis",d));
+        player.add(new Player("Donib",80, 0,"Computer Game Player",c));
+        player.add(new Player("XiaoLan",50, 3.2, "Chess",b));
+        player.add(new Player("XiaoLan",20,5.7, "Card",a));
         return player;
     }
     // choose successful sportsmen from different sports, make sure to fill Player class before starting
