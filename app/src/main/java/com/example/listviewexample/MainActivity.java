e31beba311649d3430e8500c1b993c614f5c2b12
LexingtonCV16 <1145130596@qq.com>
2019-11-20 09:05:57 +0800

    update imageview

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.listviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -28,14 +31,25 @@ public class MainActivity extends AppCompatActivity  {
         myArrayAdapter= new MyAdapter(getApplicationContext(), R.id.listview1, getPlayers());
         //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myArrayAdapter);
         listView.setAdapter(myArrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
+                String url=myArrayAdapter.getItem(position).getUrl();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
     //Todo  create a method getPlayers() that will create an array of 15 players and return the list of type List<Player>
     public ArrayList<Player> getPlayers(){
+
         ArrayList<Player> player = new ArrayList<Player>();
-        player.add(new Player("XiaoHong", 18, 2.5, "tennis"));
-        player.add(new Player("XiaoMing",80, 0,"Computer Game Player"));
-        player.add(new Player("XiaoLan",50, 3.2, "Chess"));
-        player.add(new Player("XiaoLan",20,5.7, "Card"));
+        player.add(new Player("Aron Baynes", 33, 7, "NBA Basketball", "https://www.foxsports.com/nba/aron-baynes-player-stats",-700113));
+        player.add(new Player("Reggie Bullock",28, 0,"NBA Basketball", "https://www.foxsports.com/nba/reggie-bullock-player-stats",-700103));
+        player.add(new Player("Robert Covington",29, 6, "NBA Basketball", "https://www.foxsports.com/nba/robert-covington-player-stats",-700034));
+        player.add(new Player("Derrick Favors",28,9, "NBA Basketball", "https://www.foxsports.com/nba/derrick-favors-player-stats",-700076));
+        player.add(new Player("Patty Mills", 31, 10, "NBA Basketball", "https://www.foxsports.com/nba/patty-mills-player-stats",-700016));
+        //player.add(new Player(""))
         return player;
     }
     // choose successful sportsmen from different sports, make sure to fill Player class before starting
